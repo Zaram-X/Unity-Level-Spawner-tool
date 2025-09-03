4c2cf7f27505881d7c94a057c5a86c52668cb0d7
Zaram-X <cliffizunobi@gmail.com>
2025-09-03 06:08:07 +0100

    Added spawn buttons to Level Spawner Tool
    - Implemented PrefabUtility.InstantiatePrefab for spawning prefabs
    
    - Added buttons for Coin, Enemy, and Platform
    
    - Prefabs spawn at position (0,0,0) in the scene

--- Assets/Scripts/Editor/LevelSpawnerWindow.cs
+++ Assets/Scripts/Editor/LevelSpawnerWindow.cs
@@ -23,11 +23,44 @@ public class LevelSpawnerWindow : EditorWindow
 
         // Prefab input field
         coinPrefab = (GameObject)EditorGUILayout.ObjectField("CoinP refab", coinPrefab, typeof(GameObject), false);
-        enemyPrefab = (GameObject)EditorGUILayout.ObjectField ("Enemy prefab", enemyPrefab, typeof(GameObject), false);
-        platformPrefab= (GameObject)EditorGUILayout.ObjectField ("Platform Prefab", platformPrefab, typeof(GameObject), false);
+        enemyPrefab = (GameObject)EditorGUILayout.ObjectField("Enemy prefab", enemyPrefab, typeof(GameObject), false);
+        platformPrefab = (GameObject)EditorGUILayout.ObjectField("Platform Prefab", platformPrefab, typeof(GameObject), false);
 
 
         GUILayout.Space(10);
-        GUILayout.Label("Drag your prefabs here!", EditorStyles.helpBox);
+        // GUILayout.Label("Drag your prefabs here!", EditorStyles.helpBox);
+
+        //Spawn buttons
+        if (GUILayout.Button("Spawn Coin"))
+        {
+            SpawnPrefab(coinPrefab);
+        }
+        if (GUILayout.Button("Spawn Enemy"))
+        {
+            SpawnPrefab(enemyPrefab);
+        }
+        if (GUILayout.Button("Spawn Platform"))
+        {
+            SpawnPrefab(platformPrefab);
+        }
+
+    }
+
+    private void SpawnPrefab(GameObject prefab)
+    {
+        if (prefab != null)
+        {
+            // spawn prefab at world postion (0,0,0)
+            GameObject spawned = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+            spawned.transform.position = Vector3.zero;
+
+            //mark scene dirty(so Unity knows it changed)
+            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(spawned.scene);
+             Debug.Log($"✅ Spawned: {prefab.name}");
+        }
+        else
+        {
+            Debug.LogWarning("No prefab assigned");
+        }
     }
 }
